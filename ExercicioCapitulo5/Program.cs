d1c1bd05f972c4f5914f6caaa85f29db74a4fdcf
Henrique Mette <henrique.mette@havan.com.br>
2022-09-14 19:19:04 -0300

    Feito exercicio 7

--- ExercicioCapitulo5/Program.cs
+++ ExercicioCapitulo5/Program.cs
@@ -14,6 +14,27 @@ namespace ExercicioCapitulo5
 
             float imc = peso / (altura * altura);
             Console.WriteLine(imc);
+
+            if (imc <= 18.5)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imc > 18.5 && imc < 25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                Console.WriteLine("Excesso de peso");
+            }
+            else if (imc > 30 && imc < 35)
+            {
+                Console.WriteLine("Obesidade");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade extrema");
+            }
         }
     }
 }
---
+++ ExerciciosCapitulo7/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ExerciciosCapitulo7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Numero 1
+            Console.WriteLine("Digite sua idade");
+            var idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 13 && idade < 19)
+            {
+                Console.WriteLine("Adoslecente");
+            }
+            else if (idade >= 19 && idade <= 60)
+            {
+                Console.WriteLine("Adulto");
+            }
+            else if (idade > 60)
+            {
+                Console.WriteLine("Idoso");
+            }
+            else
+            {
+                Console.WriteLine("Criança");
+            }
+
+            //Numero 2
+
+            Console.WriteLine("Quem ganhou a copa do mundo de 2014? A) Brasil B)Equador C)Alemanha D)Rússia");
+
+            var respostaUsuario = Console.ReadLine().ToUpper();
+
+            switch (respostaUsuario)
+            {
+                case "A":
+                    Console.WriteLine("Errou");
+                    break;
+
+                case "B":
+                    Console.WriteLine("Errou");
+                    break;
+
+                case "C":
+                    Console.WriteLine("Acertou");
+                    break;
+
+                case "D":
+                    Console.WriteLine("Errou");
+                    break;
+
+                default:
+                    Console.WriteLine("Errou");
+                    break;
+            }
+        }
+    }
+}
