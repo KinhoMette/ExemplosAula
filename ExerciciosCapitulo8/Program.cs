154caa1e233537804472455d109572991d602953
Henrique Mette <henrique.mette@havan.com.br>
2022-09-16 19:47:23 -0300

    Feito Exercicios caps 8 e 9

---
+++ ExerciciosCapitulo8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciciosCapitulo8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            var numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = numero; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Exercicio2();
+            Exercicio3();
+        }
+
+        private static void Exercicio2()
+        {
+            var numero = 0;
+            var contador = 0;
+            do
+            {
+                Console.WriteLine("Digite um número par");
+                numero = Convert.ToInt32(Console.ReadLine());
+                contador++;
+            } while (numero % 2 != 0);
+
+            var x = numero / 2;
+
+            Console.WriteLine(x * contador);
+        }
+
+        private static void Exercicio3()
+        {
+            var resposta = "";
+            while (resposta != "D")
+            {
+                Console.WriteLine("Qual time ganhou a copa de 2014? A) Brasil B) Belgica C) Equador D) Alemanha");
+                resposta = Console.ReadLine().ToUpper();
+
+                if (resposta != "D")
+                {
+                    Console.WriteLine("Errou");
+                }
+            }
+            Console.WriteLine("Acertou!");
+        }
+    }
+}
---
+++ ExerciciosCapitulo9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciciosCapitulo9
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var valores = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> valoresMultiplicados = new List<int>();
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.WriteLine("valores iniciais: " + valores[i]);
+
+                valoresMultiplicados.Add(valores[i] * 10);
+            }
+
+            foreach (var valor in valoresMultiplicados)
+            {
+                Console.WriteLine("valores multiplicados: " + valor);
+            }
+
+            Console.Read();
+        }
+    }
+}
